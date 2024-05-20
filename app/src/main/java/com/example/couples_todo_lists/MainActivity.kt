df4e1fb268242260e98f7c8660487bcd41af6a0e
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-05-20 17:54:23 -0300

    sistema de criar tasks

--- app/src/main/java/com/example/couples_todo_lists/MainActivity.kt
+++ app/src/main/java/com/example/couples_todo_lists/MainActivity.kt
@@ -1,40 +1,49 @@
 package com.example.couples_todo_lists
 
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var createTask_button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // paleta de cores
         // https://coolors.co/palette/210b2c-55286f-bc96e6-d8b4e2-ae759f
 
+        createTask_button = findViewById(R.id.CreateTaskButton)
+
         val database = Firebase.database
-        val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        val table = findViewById<TableLayout>(R.id.tabela)
+        for (i in 0..4) {
+            val row = TableRow(this)
+            val lp = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+            row.layoutParams = lp
+            val textView = TextView(this)
+            textView.text = "Name $i"
+            val checkbox = CheckBox(this)
+            row.addView(textView)
+            row.addView(checkbox)
+            table.addView(row, i)
+        }
 
-        myRef.addValueEventListener(object: ValueEventListener {
 
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = snapshot.getValue().toString()
-                Log.d(TAG, "Value is: $value")
-            }
+        createTask_button.setOnClickListener {
+            val intent = Intent(this, CriarTask::class.java)
+            startActivity(intent)
+        }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-
-        })
     }
 }
